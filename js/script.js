273ecfe7912822aaf8f80875d8e85ca45faaec74
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-02-02 22:47:32 +0100

    fetched data

--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
+// POP UP
+
 const actors = document.querySelectorAll(".actor_name");
 
-console.log(actors);
-
-actors.forEach((actor, index) => {
+actors.forEach((actor) => {
     actor.addEventListener("click", openPopUp);
 });
 
-function openPopUp(actor) {
+function openPopUp() {
     let popUp = document.querySelector(".pop_up");
     popUp.style.setProperty("visibility", "visible");
     let closeButton = document.querySelector(".close_button");
     closeButton.addEventListener("click", () => {
         popUp.style.setProperty("visibility", "hidden");
     });
+}
+
+// FETCHING DATA 
+
+fetch("../json/actors.json")
+    .then(res => res.json())
+    .then(function (data) {
+        handleActorList(data);
+    })
+
+function handleActorList(data) {
+    console.log(data);
+    data.forEach(showActor);
+}
+
+function showActor(actor, index) {
+    // console.log(actor);
+    //grab the template
+    const template = document.querySelector(".list_template").content;
+    // clone it
+    const copy = template.cloneNode(true);
+    // change content
+    copy.querySelector(".list_number").textContent = index + 1;
+    copy.querySelector(".actor_name").textContent = actor.fullname;
+    // grab parent
+    const parent = document.querySelector(".actorlist");
+    // append
+    parent.appendChild(copy);
+
+    // call popUp
+    
 }
